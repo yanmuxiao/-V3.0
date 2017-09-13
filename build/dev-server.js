0c10b01c52894fdc4b980158aded4b67890e4092
yanmuxiao <421559950@qq.com>
2017-09-13 18:09:14 +0800

    dev端口修改

--- build/dev-server.js
+++ build/dev-server.js
@@ -63,7 +63,8 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+// var uri = 'http://localhost:' + port
+var uri = 'http://192.168.22.112:' + port
 
 var _resolve
 var readyPromise = new Promise(resolve => {
